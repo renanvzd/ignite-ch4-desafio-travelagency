1b2296130f4017895b10c466150ebc4e55099bdb
Renan Veronez Drechsler <renanvz@outlook.com>
2021-12-14 10:55:56 -0300

    Page: Continent - Banner

---
+++ src/pages/continents.tsx
@@ -0,0 +1,26 @@
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
+
+export default function Continents() {
+  return (
+    <Box >
+      <Image
+        w="100%"
+        h={["163px", "250px", "270px", "500px"]}
+        alignContent="right"
+        src='continentBanner.png'
+        alt='Avião'
+        justifySelf="right"
+      />
+      <Flex position={"absolute"} display="absolute" w={["250px", "426px"]} ml={["30px", "60px", "150px", "200px"]} mt={["-40px", "-70px", "-80px", "-110px"]}>
+        <Text
+          fontSize={["14px", "26px", "26px", "48px"]}
+          fontWeight="600"
+          lineHeight={["30px", "54px"]}
+          color="#ffffff"
+        >
+          Europa
+        </Text>
+      </Flex>
+    </Box >
+  )
+}
